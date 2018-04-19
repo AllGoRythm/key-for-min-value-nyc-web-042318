a6fa4f3cae5af4c83d218ac5c8c3c95122a92438
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-19 21:22:14 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,8 @@ def key_for_min_value(name_hash)
   else
     name_key = name_hash.first[0]
     name_value = name_hash.first[1]
+
+    
     name_hash.each do |key,value|
     if value < name_value
       name_key = key
