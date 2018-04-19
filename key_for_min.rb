d517f5f475f02705876da687898a514c3e212591
Kurt Bauer <kurtbauer55@protonmail.com>
2018-04-19 21:09:00 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,5 @@
 require "pry"
 
 def key_for_min_value(name_hash)
-  new_array = []
-
-  name_hash.each do |key, value|
-    binding.pry
-    if value
-      new_array << value
-    end
-  end
 
 end
